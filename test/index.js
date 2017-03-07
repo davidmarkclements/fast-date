624ef91ae57bb1a45c693599621506eb734032a1
davidmarkclements <huperekchuno@googlemail.com>
2017-03-07 15:58:21 +0000

    100% cov on all versions

--- test/index.js
+++ test/index.js
@@ -60,29 +60,35 @@ test('falls back to fallback even if attempt causes throw', function (t) {
   t.end()
 })
 
-if (process.emitWarning) {
-  test('emits warning if http is loaded before fast-date', function (t) {
-    for (var k in require.cache) delete require.cache[k]
-    process.once('warning', function (msg) {
-      t.is(msg.toString(), 'Warning: For best performance, load fast-date before requiring http(s)', 'warning emitted')
-      t.end()
-    })
-    require('http')
-    require('../')
-    for (var p in require.cache) delete require.cache[p]
+test('emits warning if http is loaded before fast-date', function (t) {
+  for (var k in require.cache) delete require.cache[k]
+  if (!process.emitWarning) process.emitWarning = function polyfill(msg) {
+    process.emit('warning', 'Warning: ' + msg)
+  }
+  process.once('warning', function (msg) {
+    t.is(msg.toString(), 'Warning: For best performance, load fast-date before requiring http(s)', 'warning emitted')
+    if (process.emitWarning.name === 'polyfill') delete process.emitWarning
+    t.end()
   })
+  require('http')
+  require('../')
+  for (var p in require.cache) delete require.cache[p]
+})
 
-  test('uses module.exports.TOP_NAME in load order warning if assigned by parent', function (t) {
-    for (var k in require.cache) delete require.cache[k]
-    process.once('warning', function (msg) {
-      t.is(msg.toString(), 'Warning: For best performance, load some-parent-module before requiring http(s)', 'custom warning emitted')
-      t.end()
-    })
-    require('http')
-    require('../').TOP_NAME = 'some-parent-module'
-    for (var p in require.cache) delete require.cache[p]
+test('uses module.exports.TOP_NAME in load order warning if assigned by parent', function (t) {
+  for (var k in require.cache) delete require.cache[k]
+  if (!process.emitWarning) process.emitWarning = function polyfill(msg) {
+    process.emit('warning', 'Warning: ' + msg)
+  }
+  process.once('warning', function (msg) {
+    t.is(msg.toString(), 'Warning: For best performance, load some-parent-module before requiring http(s)', 'custom warning emitted')
+    if (process.emitWarning.name === 'polyfill') delete process.emitWarning
+    t.end()
   })
-}
+  require('http')
+  require('../').TOP_NAME = 'some-parent-module'
+  for (var p in require.cache) delete require.cache[p]
+})
 
 test('warning system falls back to console.warn in absence of process.emitWarning', function (t) {
   for (var k in require.cache) delete require.cache[k]
